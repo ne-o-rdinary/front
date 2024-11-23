a84e6cfe377c2c06e120bce4617175bd6220a712
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2024-11-23 20:53:40 +0900

    ⚙️[SETTING] : react 세팅

--- web/src/App.js
+++ web/src/App.js
@@ -4,25 +4,8 @@ import Main from "./Main";
 import Test from "./Test";
 
 const App = () => {
-  // React.useEffect(() => {
-  //   if (window.ReactNativeWebView) alert("Running inside WebView");
-  //   else alert("This feature only works inside a React Native WebView.");
-  // }, []);
-
   return (
     <div>
-      <h1>React Web App Test</h1>
-      <button
-        onClick={() => {
-          if (window.ReactNativeWebView) {
-            window.ReactNativeWebView.postMessage("Hello from Web!");
-          } else {
-            alert("ReactNativeWebView not available.");
-          }
-        }}
-      >
-        Send to React Native
-      </button>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Main />}></Route>
